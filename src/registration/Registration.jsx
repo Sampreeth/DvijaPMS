84858d062147477be58f936a15dd6ea59fdc931d
Sampreeth <sampreeth10@gmail.com>
2018-03-29 12:58:29 +0530

    complete new set now

--- src/registration/Registration.jsx
+++ src/registration/Registration.jsx
@@ -1,67 +1,15 @@
-var React         = require('react');
-var AccountFields = require('./AccountFields');
-var UserTypeFields  = require('./UserTypeFields');
-var Confirmation  = require('./Confirmation');
-var Success       = require('./Success');
-var createReactClass = require('create-react-class');
+import React from 'react'
+import { Step } from 'semantic-ui-react'
+import "../semantic/dist/semantic.min.css"
 
-var fieldValues = {
-	name     : null,
-	email    : null,
-	password : null,
-	age      : null,
-	phoneNumber:null,
-	userType: null,
-	collegeName:null,
-	usn				:null,
-	corporateName:null
+
+export default class Registration extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div></div>
+        );
+    }
 }
-
-var Registration = createReactClass({
-	getInitialState: function() {
-		return {
-			step: 1
-		}
-	},
-	saveValues: function(fields) {
-		return function() {
-		  // Remember, `fieldValues` is set at the top of this file, we are simply appending
-		  // to and overriding keys in `fieldValues` with the `fields` with Object.assign
-		  // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
-		  fieldValues = Object.assign({}, fieldValues, fields)
-		}()
-	},
-	nextStep: function() {
-		this.setState({
-		  step : this.state.step + 1
-		})
-	},
-	previousStep: function() {
-		this.setState({
-		  step : this.state.step - 1
-		})
-	},
-	render: function() {
-		switch (this.state.step) {
-			case 1:
-			  return <AccountFields fieldValues={fieldValues}
-									nextStep={this.nextStep}
-									saveValues={this.saveValues} />
-			case 2:
-			  return <UserTypeFields fieldValues={fieldValues}
-								   nextStep={this.nextStep}
-								   previousStep={this.previousStep}
-								   saveValues={this.saveValues} />
-			case 3:
-				return <Confirmation fieldValues={fieldValues}
-									 nextStep={this.nextStep}
-								   previousStep={this.previousStep}
-								   saveValues={this.saveValues} />
-			case 4:
-			  return <Success fieldValues={fieldValues} />
-			default:
-				return null;
-		}
-	}
-});
-module.exports = Registration
